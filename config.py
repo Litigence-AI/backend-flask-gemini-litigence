036dd047d2f50059787754648f3765f072721184
KarthiDreamr <karthidreamr@gmail.com>
2025-03-09 18:39:20 +0530

    chore: replaced with newer ai model

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ HOST = "0.0.0.0"
 # Google AI configuration
 PROJECT_ID = os.environ.get('PROJECT_ID')
 LOCATION = os.environ.get('LOCATION')
-MODEL_NAME = "gemini-1.5-pro-002"
+MODEL_NAME = "gemini-2.0-flash"
 
 # System prompts
 LAW_ASSISTANT_INSTRUCTION = """You are Litigence AI 🤖⚖️ an Indian law legal AI Assistant
